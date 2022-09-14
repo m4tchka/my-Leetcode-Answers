03e1f660f253d6df0225678c6b8f932f35bb0509
m4tchka <91279022+m4tchka@users.noreply.github.com>
2022-09-14 15:04:19 +0100

    number-of-steps

---
+++ Easy/number-of-steps-to-reduce-a-number-to-zero.js
@@ -0,0 +1,24 @@
+"https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/submissions/"
+
+/* 
+Notes: Very slow for some reason ? Maybe use recursion idk
+
+Runtime: 100 ms, faster than 30.67% of JavaScript online submissions for Number of Steps to Reduce a Number to Zero.
+Memory Usage: 41.7 MB, less than 88.66% of JavaScript online submissions for Number of Steps to Reduce a Number to Zero.
+*/
+
+var numberOfSteps = function (num) {
+    let count = 0;
+    while (num > 0) {
+        num % 2 == 0
+            ? () => {
+                  num /= 2;
+                  count++;
+              }
+            : () => {
+                  num--;
+                  count++;
+              };
+    }
+    return count;
+};
