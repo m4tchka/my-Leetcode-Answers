767df6682aadac6d43f136ae08a8ede9a937c6bd
m4tchka <91279022+m4tchka@users.noreply.github.com>
2022-10-22 17:38:04 +0100

    single-number other

--- Easy/single-number.js
+++ Easy/single-number.js
@@ -18,7 +18,25 @@ var singleNumber = function (nums) {
         }
     }
 };
-
 /* 
     Check if the index of each entry is the same as the last index of that entry (i.e. there is only a single occurence) and return that number 
+*/
+/////////////////
+/* 
+    Runtime: 112 ms, faster than 61.21% of JavaScript online submissions for Single Number.
+    Memory Usage: 46.9 MB, less than 34.23% of JavaScript online submissions for Single Number.
+*/
+var singleNumber = function (nums) {
+    let numObj = {};
+    for (let i = 0; i < nums.length; i++) {
+        numObj[nums[i]] = numObj[nums[i]] ? 2 : 1;
+    }
+    for (let key in numObj) {
+        if (numObj[key] == 1) {
+            return key;
+        }
+    }
+};
+/* 
+Create an object and put each num as an entry, return the entry that has 1 as value
 */
