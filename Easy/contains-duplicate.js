9bc1ec33442638fa17c53a244683a085afd8af3d
m4tchka <91279022+m4tchka@users.noreply.github.com>
2022-11-14 18:27:58 +0000

    contains-duplicate (Set instead of Object)

--- Easy/contains-duplicate.js
+++ Easy/contains-duplicate.js
@@ -6,6 +6,10 @@
  * Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
  */
 
+/* 
+    NOTE: Set is much faster - it only has 1 "value" property and there are no keys.
+*/
+
 var containsDuplicate = function (nums) {
     let obj = {};
     for (let i = 0; i < nums.length; i++) {
@@ -17,3 +21,14 @@ var containsDuplicate = function (nums) {
     return false;
 };
 
+var containsDuplicate = function(nums) {
+    let s = new Set()
+    for (let i = 0; i < nums.length; i ++) {
+       if (s.has(nums[i])) {
+        return true;
+       } else {
+        s.add(nums[i]);
+       }
+    }
+    return false
+};
