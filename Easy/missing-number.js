1a1678fbf86756c35bc85ae152f2d7dbbab1b24c
m4tchka <91279022+m4tchka@users.noreply.github.com>
2022-11-26 19:48:26 +0000

    missing-number (slow)

---
+++ Easy/missing-number.js
@@ -0,0 +1,14 @@
+"https://leetcode.com/problems/missing-number";
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ * Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+ */
+var missingNumber = function (nums) {
+    nums.sort((a, b) => a - b); // [0, 1, 3]
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== i) return i;
+    }
+    return nums.length;
+};
