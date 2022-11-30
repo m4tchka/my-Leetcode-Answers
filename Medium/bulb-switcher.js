c459053968348133b85963eddf979913a8c914f7
m4tchka <91279022+m4tchka@users.noreply.github.com>
2022-11-30 12:05:32 +0000

    bulb-switcher (med) (slow)

---
+++ Medium/bulb-switcher.js
@@ -0,0 +1,17 @@
+"https://leetcode.com/problems/bulb-switcher/submissions/852279095/"
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var bulbSwitch = function (n) {
+    if (n === 0) return 0;
+    let currSq = 1;
+    while ((currSq + 1) ** 2 <= n) {
+        console.log({ currSq });
+        currSq++;
+    }
+    return currSq;
+};
+// Number of bulbs left on is simply HOW MANY square numbers there are from 0 to (including) n.
+// Only square numbers have an ODD number of factors - so they are toggled an odd number of times. ( 8 toggled at loops [1,2,4,8] vs 16 at [1,2,4,8,16] )
+// Since they start at off, this means at the end bulbs at those positions are left toggled on at the end.
