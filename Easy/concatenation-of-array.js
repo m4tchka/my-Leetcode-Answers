493681deaab4b114286bb0113fe71e03072770b3
m4tchka <91279022+m4tchka@users.noreply.github.com>
2023-03-26 16:05:30 +0100

    cocnatenation of array

---
+++ Easy/concatenation-of-array.js
@@ -0,0 +1,11 @@
+"https://leetcode.com/problems/concatenation-of-array/"
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ * Given an integer array nums of length n, you want to create an array ans of length 2n where ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
+ * Specifically, ans is the concatenation of two nums arrays.
+ * Return the array ans.
+ */
+var getConcatenation = function(nums) {
+    return nums.concat(nums)
+};
