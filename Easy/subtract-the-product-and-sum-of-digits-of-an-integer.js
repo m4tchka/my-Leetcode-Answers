1c41b7526b9fa5b44335ecbe044df5d47ea60f5f
m4tchka <91279022+m4tchka@users.noreply.github.com>
2023-04-30 23:08:29 +0100

    subtract product and sum of digits alt

--- Easy/subtract-the-product-and-sum-of-digits-of-an-integer.js
+++ Easy/subtract-the-product-and-sum-of-digits-of-an-integer.js
@@ -4,7 +4,7 @@
  * Given an integer number n, return the difference between the product of its digits and the sum of its digits.
  * @param {number} n
  * @return {number}
-*/
+ */
 
 /* 
     NOTE: Straightforward ?
@@ -22,10 +22,23 @@ var subtractProductAndSum = function (n) {
     });
     return prod - sum;
 };
-
 /* 
     Convert n to an array of its digits
     Reduce the array to the product of its entries & store it as variable prod
     Reduce the array to the sum of its entries & store it as variable sum
     Return the difference of prod and sum
+*/
+var subtractProductAndSum = function (n) {
+    str = n.toString();
+    let [sum, prod] = [0, 1];
+    for (let i = 0; i < str.length; i++) {
+        sum += Number(str[i]);
+        prod *= Number(str[i]);
+    }
+    return prod - sum;
+};
+/* 
+    Convert n to a string so that it can be looped through using a for loop on it's indexes.
+    Create variables for sum and product, loop through the string and add and multiply each number to its respective variable
+    Return the difference
 */
